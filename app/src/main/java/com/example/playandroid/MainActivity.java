563b30025697d3883e4b59bd3d4e9e2d474587a8
18822835767 <1165407654@qq.com>
2020-07-14 10:41:58 +0800

    toolbar写完了一部分

--- app/src/main/java/com/example/playandroid/MainActivity.java
+++ app/src/main/java/com/example/playandroid/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.playandroid;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.app.Notification;
 import android.os.Bundle;
+import android.util.LruCache;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +15,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        //ToolBar左侧显示出导航栏
+        ActionBar actionBar = getSupportActionBar();
+        if(actionBar != null){
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeAsUpIndicator(R.drawable.navi_white);
+        }
     }
 }
