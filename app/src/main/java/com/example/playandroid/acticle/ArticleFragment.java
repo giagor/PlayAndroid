7535e3f9bdf7c462f147d3e2534be3c2d4dc71ea
18822835767 <1165407654@qq.com>
2020-07-15 21:29:16 +0800

    写完了recyclerview

--- app/src/main/java/com/example/playandroid/acticle/ArticleFragment.java
+++ app/src/main/java/com/example/playandroid/acticle/ArticleFragment.java
@@ -1,27 +1,50 @@
 package com.example.playandroid.acticle;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
+import com.example.playandroid.R;
+import com.example.playandroid.adapter.ArticleAdapter;
 import com.example.playandroid.entity.Article;
 
 import java.util.List;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class ArticleFragment extends Fragment implements ArticleContract.OnView{
     private ArticleContract.Presenter mArticlePresenter;
-
+    private RecyclerView mRecyclerView;
+    private View mView;
+    
+    @Nullable
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, 
+                             @Nullable Bundle savedInstanceState) {
+        mView = inflater.inflate(R.layout.acticle_fragment,container,false);
+        
+        initData();
+        
+        return mView;
+    }
+
+    private void initData(){
         new ArticlePresenter(this);
+        
+        mRecyclerView = mView.findViewById(R.id.recycler_view);
     }
 
     @Override
     public void onResume() {
         super.onResume();
-        mArticlePresenter.start();
+        if(mArticlePresenter != null){
+            mArticlePresenter.start();
+        }
     }
 
     @Override
@@ -31,7 +54,10 @@ public class ArticleFragment extends Fragment implements ArticleContract.OnView{
 
     @Override
     public void onSuccess(List<Article> articles) {
-        
+        RecyclerView.LayoutManager manager = new LinearLayoutManager(getContext());
+        mRecyclerView.setLayoutManager(manager);
+        ArticleAdapter adapter = new ArticleAdapter(articles);
+        mRecyclerView.setAdapter(adapter);
     }
 
     @Override
---
+++ app/src/main/java/com/example/playandroid/adapter/ArticleAdapter.java
@@ -0,0 +1,56 @@
+package com.example.playandroid.adapter;
+
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.playandroid.R;
+import com.example.playandroid.entity.Article;
+
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ArticleAdapter extends RecyclerView.Adapter<ArticleAdapter.ViewHolder> {
+    private List<Article> mArticles;
+    
+    static class ViewHolder extends RecyclerView.ViewHolder{
+        TextView mTitle;
+        TextView mAuthor;
+        
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            mTitle = itemView.findViewById(R.id.title);
+            mAuthor = itemView.findViewById(R.id.author);
+        }
+    }
+
+    public ArticleAdapter(List<Article> articles) {
+        mArticles = articles;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.article_item,parent,false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Article article = mArticles.get(position);
+        holder.mTitle.setText(article.getTitle());
+        holder.mAuthor.setText(article.getAuthor());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mArticles.size();
+    }
+
+   
+}
