5afa3516e73b4158c2e1f5364504d764398304ed
18822835767 <1165407654@qq.com>
2020-07-23 11:11:51 +0800

    写了子体系的toolbar

--- app/src/main/java/com/example/playandroid/frame/frame_child/FrameChildActivity.java
+++ app/src/main/java/com/example/playandroid/frame/frame_child/FrameChildActivity.java
@@ -1,11 +1,14 @@
 package com.example.playandroid.frame.frame_child;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.MenuItem;
 
 import com.example.playandroid.R;
 import com.example.playandroid.entity.Frame;
@@ -17,24 +20,50 @@ public class FrameChildActivity extends AppCompatActivity {
 
     private static final String TAG = "FrameChildActivity";
     private Frame mFrame;
+    private Toolbar mToolbar;
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_frame_child);
+        
+        initView();
+        initData();
+        setActionBar();
+    }
+    
+    private void initView(){
+        mToolbar = findViewById(R.id.toolbar);
     }
     
     private void initData(){
         mFrame = (Frame) getIntent().getSerializableExtra(FRAME);
-        if(mFrame != null){
-            Log.d(TAG, "initData: "+mFrame.getName());
-        }
     }
     
+    private void setActionBar(){
+        setSupportActionBar(mToolbar);
+        
+        ActionBar actionBar = getSupportActionBar();
+        if(actionBar != null){
+            //显示出返回的箭头
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            //设置标题
+            actionBar.setTitle(mFrame.getName());
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == android.R.id.home){
+            //点击返回箭头时，销毁该活动
+            finish();
+        }
+        return true;
+    }
+
     public static void actionStart(Context context, Frame frame){
         Intent intent = new Intent(context, FrameChildActivity.class);
         intent.putExtra(FRAME,frame);
-        Log.d(TAG, "actionStart: "+frame.getName());
         context.startActivity(intent);
     }
 }
